2569619be2e55847908ec24b469def1f7f6eee4d
Franco-RD <franco.romano.data@gmail.com>
2023-12-15 16:43:57 -0300

    Ejercicios de prueba agregados

--- main.py
+++ main.py
@@ -18,3 +18,20 @@ app = Flask(__name__)  #El name es para pasarle el parametro por otro medio de e
 @app.route("/hola")  #El decorador sirve para llamar a la funcion que sigue luego de llamar al link con el / que defina. En este caso si pongo http://127.0.0.1:5000/hola va a llamar a la funcion hola_mundo
 def hola_mundo():
     return "Hola mundo flask, esto es flask, hoy es viernes"
+
+#Ej: crear una ruta que devuelva una lista de frutas, el path seria /frutas
+
+@app.route("/frutas")
+def lista_frutas():
+    list_fruta = ['Platano', 'Fresa', 'Piña', 'Melon', 'Naranja']
+    return list_fruta
+
+@app.route("/nombre/<name>")  #Lo que esta entre <> lo toma como una variable que se puede usar mas abajo
+def tunombre(name):
+    return f"hola {name} como estas"
+
+#Ej: realizar una ruta que devuelva el cuadrado de un numero dado, /numero/<parametro>
+
+@app.route("/numero/<int:parametro>")  #Se pone <int:parametro> porque por default el parametro es tomado como str, por lo que daria error al hacer el cuadrado
+def cuadrado(parametro):
+    return f"El parametro de {parametro} es {parametro*parametro}"
